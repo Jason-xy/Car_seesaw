708a375b43ee9c446d44d6ccc9dbfc97bf15d202
JasonXY <1462357003@qq.com>
2020-12-28 16:50:28 +0800

    1.速度开环；2.电机闭环；3.远程pid调试

---
+++ F411CEU6-DMPRefactor/APP/src/control.c
@@ -0,0 +1,232 @@
+/**
+  ******************************************************************************
+  * 文件名程: control.c 
+  * 作    者: Jason_xy
+  * 个人博客：https://jason-xy.cn
+  * 版    本: V1.0
+  * 编写日期: 2020-12-15
+  * 功    能: PID控制
+  ******************************************************************************
+  * 说明：
+  * 1.需要提前配置好gy-86。
+  * 2.需要提前配置好电机。
+  * 
+  * 功能：
+  * 1.角度环控制。
+  * 2.电机内环PID。
+  * 3.速度外环PID。
+  ******************************************************************************
+  */
+ #include "control.h"
+
+unsigned char SpeedControlCount;
+unsigned char SpeedControlPeriod;
+
+//中断触发通道
+volatile HAL_TIM_ActiveChannel ActiveChannel[4]={	HAL_TIM_ACTIVE_CHANNEL_1, 
+                                                    HAL_TIM_ACTIVE_CHANNEL_2, 
+                                                    HAL_TIM_ACTIVE_CHANNEL_3, 
+                                                    HAL_TIM_ACTIVE_CHANNEL_4};
+//TIM通道
+volatile uint8_t Channel[4] = { 0x00000000U, //TIM_CHANNEL_1
+                                0x00000004U, //TIM_CHANNEL_2
+                                0x00000008U, //TIM_CHANNEL_3
+                                0x0000000CU};//TIM_CHANNEL_4
+
+//全局变量
+PID AngleRingPID={0.8,0,1}; //3，-1，1
+PID SpeedRingPID={0,0,0};   //速度开环
+PID MotorRingPID[4]={{10,0.9,0},{10,0.9,0},{10,0.9,0},{10,0.9,0}};    //电机闭环
+//角度控制参数
+float GyroAngleSpeed=0;
+float GyroAngleSpeedBefore=0;
+float GyroAccle=0;
+float CarAngle=0;
+float CarAngleOld=0;
+
+//电机控制参数
+float AngleControlOut=0;
+float SpeedControlOut[4]={0};
+float SpeedControlOutOld[4]={0};
+float SpeedControlOutNew[4]={0};
+float MotorOut[4]={0};
+float ErrorRrev[4]={0};
+float PWMBais[4]={0};
+float PWM[4]={0};
+
+//速度控制参数
+short  MotorPulse[4]={0};
+int  MotorPulseOld[4]={0};
+int  MotorPulseSigma[4]={0};
+
+float CarSpeed[4]={0};
+float CarSpeedSet=0;
+float CarSpeedOld[4]={0};
+float CarPosition[4]={0};
+
+//电机脉冲捕获
+uint8_t CapFlag[4] = { 0 };
+uint32_t CapVal[4] = { 0 };																	
+float SpeedOfWheel[4]={0};    //°/s
+int cap = 0;
+
+//量程归一化处理
+float Scale(float input, float inputMin, float inputMax, float outputMin, float outputMax)
+{ 
+  float output;
+  if (inputMin < inputMax)
+    output = (input - inputMin) / ((inputMax - inputMin) / (outputMax - outputMin));
+  else
+    output = (inputMin - input) / ((inputMin - inputMax) / (outputMax - outputMin));
+  if (output > outputMax)
+    output = outputMax;
+  else if (output < outputMin)
+    output = outputMin;
+  return output;
+}
+
+
+//角度环计算
+void AngleCalculate(void)
+{
+    //角速度
+	//范围为2000deg/s时，换算关系：131.2 LSB/(deg/s)
+	//使用显式读出，无需单位换算。
+	if(roll-CarAngle<0.001f||CarAngle-roll<0.001f)
+	CarAngle=roll;
+	GyroAngleSpeed=(CarAngle*1000-CarAngleOld*1000)/5.0f;
+	GyroAccle=(GyroAngleSpeed-GyroAngleSpeedBefore)/0.05f;
+
+    //角度【时间差根据实际测量计算】
+    //CarAngle=CarAngle+GyroAngleSpeed*0.248f;
+	
+	//直接使用DMP姿态解算数据
+	CarAngleOld=CarAngle;                        //[-5~5]
+	GyroAngleSpeedBefore=GyroAngleSpeed;
+}
+
+//角度环控制
+void AngleControl(void)
+{
+    AngleControlOut=CarAngle*AngleRingPID.P+\
+		GyroAccle*AngleRingPID.I+\
+    GyroAngleSpeed*AngleRingPID.D/10.0f;         //数量级10^1
+}
+
+//电机速度捕获
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+	//选择对应定时器，对应通道
+	for(int i=0;i<4;i++){
+		if(htim->Channel == ActiveChannel[i])
+		{
+			cap=1;
+			switch(CapFlag[i])
+			{//读取当前通道的状态				
+				case 0://没有读到过中断
+					__HAL_TIM_SET_COUNTER(htim,0);  //计数器置0
+					CapFlag[i]++;  //修改捕获状态为捕获过一次
+					break;
+				case 1:
+					CapVal[i] = HAL_TIM_ReadCapturedValue(htim,Channel[i]); //读取计数器值	
+					SpeedOfWheel[i]=375000/CapVal[i];//360*10^6/(Cap*960)
+					CapFlag[i] = 0;    //重置捕获状
+					break;
+			}	
+		}
+		if(cap)
+        {
+			cap = 0;
+            break;
+        }
+	}
+}
+
+//速度开环计算									//目标转动速度0
+void SpeedControl(void)
+{
+  float fP[4],fI[4];   	
+	float fDelta[4];
+	for(int i=0;i<4;i++)
+	{	
+		CarSpeed[i] = SpeedOfWheel[0]; 
+		CarSpeed[i] = 0.7f * CarSpeedOld[i] + 0.3f * CarSpeed[i] ;//低通滤波，使速度更平滑
+		CarSpeedOld[i] = CarSpeed[i];                                     
+		
+		fDelta[i] = CAR_SPEED_SET;
+		fDelta[i] -= CarSpeed[i];            //数量级10
+		
+		fP[i] = fDelta[i] * (SpeedRingPID.P);
+		fI[i] = fDelta[i] * (SpeedRingPID.I/10.0f);
+
+		CarPosition[i] += fI[i];
+		
+		//积分上限设限
+		if((short)CarPosition[i] > CAR_POSITION_MAX)    CarPosition[i] = CAR_POSITION_MAX;
+		if((short)CarPosition[i] < CAR_POSITION_MIN)    CarPosition[i] = CAR_POSITION_MIN;
+		
+		SpeedControlOutOld[i] = SpeedControlOutNew[i];
+		SpeedControlOutNew[i] = fP[i] + CarPosition[i];
+	}
+}
+
+//速度开环控制函数
+void SpeedControlOutput(void)
+{
+  float fValue[4];
+	
+	for(int i=0;i<4;i++)
+	{
+		fValue[i] = SpeedControlOutNew[i] - SpeedControlOutOld[i] ;
+		SpeedControlOut[i] = fValue[i] * (SpeedControlPeriod + 1) / SPEED_CONTROL_PERIOD + SpeedControlOutOld[i];
+	}		
+}
+
+//速度闭环计算 4路电机
+float SpeedInnerControlCalculate(short Pulse,int Target,int MotorNum)
+{
+	int Error;
+	Error=Pulse-Target;
+	PWMBais[MotorNum]=MotorRingPID[MotorNum].P*(Error-ErrorRrev[MotorNum])+MotorRingPID[MotorNum].I*Error;
+	ErrorRrev[MotorNum]=Error;
+	PWM[MotorNum]+=PWMBais[MotorNum];
+	if(PWM[MotorNum]>100)PWM[MotorNum]=100;
+	if(PWM[MotorNum]<-100)PWM[MotorNum]=-100;
+	return PWM[MotorNum];
+}
+
+//速度闭环控制
+void SpeedInnerControl(void)
+{
+	for(int i=0;i<4;i++)
+	{
+		MotorOut[i]=SpeedInnerControlCalculate(Scale(SpeedOfWheel[i], -300, 300, -100, 100),MotorOut[i],i);
+		SetMotorDutyCycle(MotorOut[i],Channel[i]);
+	}
+}
+
+//电机输出
+void MotorOutput(void)
+{
+	for(int i=0;i<4;i++)
+	{
+    MotorOut[i]=AngleControlOut*10.0f-SpeedControlOut[i];
+	MotorOut[i]=Scale(MotorOut[i], -300, 300, -100, 100);
+
+    //添加死区常数
+    if(MotorOut[i]>1.4f)
+        MotorOut[i]+=MOTOR_OUT_DEAD_VAL+4;
+    else if(MotorOut[i]<-1.4f)
+        MotorOut[i]-=MOTOR_OUT_DEAD_VAL;
+
+    //饱和处理
+    if(MotorOut[i]>MOTOR_OUT_MAX)
+        MotorOut[i]=MOTOR_OUT_MAX;
+    if(MotorOut[i]<MOTOR_OUT_MIN)
+        MotorOut[i]=MOTOR_OUT_MIN;
+
+    SetMotorDutyCycle(MotorOut[i],Channel[i]);
+	}
+	SpeedInnerControl();//闭环输出
+}
+
---
+++ F411CEU6-DMPRefactor/Core/Src/stm32f4xx_it.c
@@ -0,0 +1,302 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32f4xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2020 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f4xx_it.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "oled.h"
+#include "motor.h"
+#include "gy-86.h"
+#include "esp8266.h"
+#include "control.h"
+#include "data_transfer.h"
+#include "inv_mpu.h"
+#include "inv_mpu_dmp_motion_driver.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+ short MainEventCount=0;
+ int test0,test1,test2,test3,test4,test5,test6;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* External variables --------------------------------------------------------*/
+extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim3;
+extern UART_HandleTypeDef huart1;
+/* USER CODE BEGIN EV */
+
+/* USER CODE END EV */
+
+/******************************************************************************/
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+  while (1)
+  {
+  }
+  /* USER CODE END NonMaskableInt_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+
+  /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Memory management fault.
+  */
+void MemManage_Handler(void)
+{
+  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+
+  /* USER CODE END MemoryManagement_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+    /* USER CODE END W1_MemoryManagement_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Pre-fetch fault, memory access fault.
+  */
+void BusFault_Handler(void)
+{
+  /* USER CODE BEGIN BusFault_IRQn 0 */
+
+  /* USER CODE END BusFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
+    /* USER CODE END W1_BusFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+  /* USER CODE BEGIN UsageFault_IRQn 0 */
+
+  /* USER CODE END UsageFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+    /* USER CODE END W1_UsageFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+  /* USER CODE BEGIN SVCall_IRQn 0 */
+
+  /* USER CODE END SVCall_IRQn 0 */
+  /* USER CODE BEGIN SVCall_IRQn 1 */
+
+  /* USER CODE END SVCall_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
+void DebugMon_Handler(void)
+{
+  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+
+  /* USER CODE END DebugMonitor_IRQn 0 */
+  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+
+  /* USER CODE END DebugMonitor_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+  /* USER CODE BEGIN PendSV_IRQn 0 */
+
+  /* USER CODE END PendSV_IRQn 0 */
+  /* USER CODE BEGIN PendSV_IRQn 1 */
+
+  /* USER CODE END PendSV_IRQn 1 */
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+/******************************************************************************/
+/* STM32F4xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32f4xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles TIM1 update interrupt and TIM10 global interrupt.
+  */
+void TIM1_UP_TIM10_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 0 */
+  //1ms进入一次中断。
+	MainEventCount++;
+	test0=TIM3->CNT;
+	mpu_dmp_get_data(&pitch,&roll,&yaw);
+	test0=TIM3->CNT-test0;
+	if(MainEventCount>=5)
+	{
+		MainEventCount=0;
+		test1=TIM3->CNT;
+		SpeedInnerControl();//速度闭环控制
+		test1=TIM3->CNT-test1;
+	}
+	else if(MainEventCount==1)
+	{	test2=TIM3->CNT;				
+		AngleCalculate();			 //角度环计算函数，每5ms执行一次
+		test2=TIM3->CNT-test2;
+	}
+	else if(MainEventCount==2)
+	{	test3=TIM3->CNT;
+		AngleControl();				 //角度环控制函数，每5ms执行一次
+		test3=TIM3->CNT-test3;
+	}
+	else if(MainEventCount==3)
+	{
+		SpeedControlCount++;
+		SpeedControlPeriod++;
+    	if(SpeedControlCount >= 5)//25ms
+    	{	test4=TIM3->CNT;	
+      		SpeedControl();          //速度控制函数，每25ms调用一次
+			SpeedControlOutput();
+      		SpeedControlCount=0;
+			SpeedControlPeriod=0;
+			test4=TIM3->CNT-test4;
+    	}
+	}
+	else if(MainEventCount==4)
+	{   test6=TIM3->CNT;
+		MotorOutput();	 		//电机输出函数，每5ms执行一次
+		test6=TIM3->CNT-test6;
+	}
+  /* USER CODE END TIM1_UP_TIM10_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim1);
+  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
+
+  /* USER CODE END TIM1_UP_TIM10_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM3 global interrupt.
+  */
+void TIM3_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM3_IRQn 0 */
+	
+  /* USER CODE END TIM3_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim3);
+  /* USER CODE BEGIN TIM3_IRQn 1 */
+
+  /* USER CODE END TIM3_IRQn 1 */
+}
+
+/**
+  * @brief This function handles USART1 global interrupt.
+  */
+void USART1_IRQHandler(void)
+{
+  /* USER CODE BEGIN USART1_IRQn 0 */
+
+  /* USER CODE END USART1_IRQn 0 */
+  HAL_UART_IRQHandler(&huart1);
+  /* USER CODE BEGIN USART1_IRQn 1 */
+  int timeout=0;
+	while(HAL_UART_Receive_IT(&huart1, (u8 *)aRxBuffer, RXBUFFERSIZE) != HAL_OK)//一次处理完成之后，重新开启中断并设置RxXferCount为1
+	{
+	 timeout++; //超时处理
+	 if(timeout>HAL_MAX_DELAY) break;	
+	}
+  /* USER CODE END USART1_IRQn 1 */
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
